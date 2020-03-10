ca6a1d1045e8b6ca2c0576d4964cf7188c59b198
Florian <florian.fort0@gmail.com>
2020-03-10 10:41:28 +0100

    connexion

--- controller/user.php
+++ controller/user.php
@@ -3,20 +3,19 @@
 class User {
 	
 	public function __construct() {
+    if($safeData->post['connexion'] !== null){
+      $this->connexion();
 
 	}
 
   public function connexion(){
     global $safeData;
 
-    if($safeData->post['connexion'] !== null){
-      if ($safeData->post['identifiant'] == 'jean_forteroche' AND $safeData->post['mdp'] === 'openclassroom'){
-        
+    if (isset($safeData->post['identifiant'] == 'jean_forteroche') AND isset($safeData->post['mdp'] === 'openclassroom')){
+          
+        $admin = new Back();
 
-
-      }
+        $admin->accueil();
     } 
   }
-
-
 }
